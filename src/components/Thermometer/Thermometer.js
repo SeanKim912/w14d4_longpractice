32979194a70c8ec0ca974f6e573840ff75eccc7f
Sean <seankim912@yahoo.com>
2022-12-01 18:01:10 -0600

    temp slider

--- src/components/Thermometer/Thermometer.js
+++ src/components/Thermometer/Thermometer.js
@@ -1,14 +1,21 @@
+import {useContext, useState} from "react";
 import ReactSlider from "react-slider";
 import './Thermometer.css';
+import {ClimateContext} from '../../context/ClimateContext';
 
 function Thermometer() {
+  const { temperature, setTemperature } = useContext(ClimateContext);
+  // const [val, setVal] = useState(temperature);
   return (
     <section>
       <h2>Thermometer</h2>
-      <div className="actual-temp">Actual Temperature: {"x"}°F</div>
+      <div className="actual-temp">Actual Temperature: {temperature}°F</div>
       <ReactSlider
-        value={40}
-        onAfterChange={(val) => {}}
+        value={temperature}
+        // onChange={(val) => {setVal(val)}}
+        onAfterChange={(e, state) => {console.log(e, state)}}
+        // onAfterChange={(e) => {setTemperature(e.target.value)}}
+
         className="thermometer-slider"
         thumbClassName="thermometer-thumb"
         trackClassName="thermometer-track"
@@ -18,7 +25,7 @@ function Thermometer() {
         max={120}
         renderThumb={(props, state) => <div {...props}>{state.valueNow}</div>}
         renderTrack={(props, state) => (
-          <div {...props} index={state.index}></div>
+          <div {...props} index={state.index}>{}</div>
         )}
         invert
         pearling
